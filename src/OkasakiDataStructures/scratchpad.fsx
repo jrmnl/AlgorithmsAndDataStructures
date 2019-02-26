709ad8757926a2cdb1059447118e53dc5b2c49b8
Vladislav Timakov <vtimakov@gotrg.com>
2019-02-27 02:14:58 +0300

    Rename folder and implement CustomList.update

---
+++ src/OkasakiDataStructures/scratchpad.fsx
@@ -0,0 +1,27 @@
+﻿#load "CustomList.fs"
+
+open OkasakiDataStructures.CustomList
+
+let randomInt =
+    let rnd = System.Random()
+    fun () -> rnd.Next()
+
+let consRandom list =
+    cons (randomInt()) list
+
+let createFourElementsList() =
+    let rnd = System.Random()
+    empty
+    |> consRandom
+    |> consRandom
+    |> consRandom
+    |> consRandom
+
+let firstList = createFourElementsList()
+let secondList = createFourElementsList()
+
+let head = head firstList
+let tail = tail firstList
+
+let concated = firstList ++ secondList
+concated
