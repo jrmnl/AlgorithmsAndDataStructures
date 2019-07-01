903862f4467fe52c1542899cb061af28b57c160f
Vladislav Timakov <jrmnnl@gmail.com>
2019-07-01 20:49:11 +0300

    Add linear search

---
+++ src/IntroductionToAlgorithmsByCormen/Searches.fs
@@ -0,0 +1,12 @@
+﻿namespace IntroductionToAlgorithmsByCormen
+
+
+module LinearSearch =
+    let tryFindIndex (element:'T) (array:'T[]) =
+        let rec loop i =
+            if (i >= array.Length)
+            then None
+            elif (array.[i] = element)
+            then Some i
+            else loop (i + 1)
+        loop 0
---
+++ src/Tests/Searches.fs
@@ -0,0 +1,10 @@
+﻿module Searches
+
+open FsCheck.Xunit
+open IntroductionToAlgorithmsByCormen
+
+[<Property>]
+let ``Linear search works correctly`` element (array:int[]) =
+    let expected = array |> Array.tryFindIndex (fun x -> x = element)
+    let result = array |> LinearSearch.tryFindIndex element
+    expected = result
