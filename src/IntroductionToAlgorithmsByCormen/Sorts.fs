0ec5407ede423023dc8e2672ee276303c100a9c3
Vladislav Timakov <jrmnnl@gmail.com>
2019-07-01 20:31:16 +0300

    Add descending sort to insertion sort

--- src/IntroductionToAlgorithmsByCormen/Sorts.fs
+++ src/IntroductionToAlgorithmsByCormen/Sorts.fs
@@ -1,13 +1,19 @@
 ﻿namespace IntroductionToAlgorithmsByCormen
 
 module InsertionSort =
-    let sort (array:'T[]) =
+    let private sort compare array =
         let arr = Array.copy array
         for i = 1 to arr.Length - 1 do
             let key = arr.[i]
             let mutable j = i - 1
-            while j >= 0 && arr.[j] > key do
+            while j >= 0 && compare arr.[j] key do
                 arr.[j + 1] <- arr.[j]
                 j <- j - 1
             arr.[j + 1] <- key
         arr
+        
+    let sortAsc array =
+        array |> sort (fun x y -> x > y)
+
+    let sortDesc array =
+        array |> sort (fun x y -> x < y)
--- src/Tests/Sorts.fs
+++ src/Tests/Sorts.fs
@@ -7,11 +7,20 @@ let private sameSeq seq1 seq2 =
     let compare = Seq.compareWith Operators.compare
     compare seq1 seq2 = 0
 
-let private isSorted sorted =
-    let expected = Seq.sort sorted
-    expected |> sameSeq sorted
+let private isSorted sorter array  = 
+    let expected = sorter array
+    expected |> sameSeq array
+
+let private isAscSorted = isSorted Seq.sort
+
+let private isDescSorted = isSorted Seq.sortDescending
 
 [<Property>]
-let ``Insertion sort works correctly`` (unsorted:int[]) =
-    InsertionSort.sort unsorted
-    |> isSorted
+let ``Insertion sort works correctly (Ascending)`` (unsorted:int[]) =
+    InsertionSort.sortAsc unsorted
+    |> isAscSorted
+    
+[<Property>]
+let ``Insertion sort works correctly (Descending)`` (unsorted:int[]) =
+    InsertionSort.sortDesc unsorted
+    |> isDescSorted
