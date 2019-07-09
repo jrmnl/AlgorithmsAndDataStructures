0a272fdbd6265cc8575406213b767f4bb2721460
Vladislav Timakov <jrmnnl@gmail.com>
2019-07-10 02:18:05 +0300

    Add binary search

--- src/IntroductionToAlgorithmsByCormen/Searches.fs
+++ src/IntroductionToAlgorithmsByCormen/Searches.fs
@@ -10,3 +10,21 @@ module LinearSearch =
             then Some i
             else loop (i + 1)
         loop 0
+
+module BinarySearch =
+    let rec private find (start:int) finish (element:'T) (array:'T[]) =
+        let len = start + finish
+        let mid = len / 2
+        if (array.[mid] = element)
+        then Some mid
+        elif (mid <> start && array.[mid] > element)
+        then array |> find start (mid - 1) element
+        elif (mid <> finish && array.[mid] < element)
+        then array |> find (mid + 1) finish element
+        else None
+
+    /// Find element in sorted array
+    let tryFindIndex (element:'T) (array:'T[]) = 
+        if (array.Length = 0)
+        then None
+        else array |> find 0 (array.Length - 1) element
--- src/Tests/Searches.fs
+++ src/Tests/Searches.fs
@@ -8,3 +8,15 @@ let ``Linear search works correctly`` element (array:int[]) =
     let expected = array |> Array.tryFindIndex (fun x -> x = element)
     let result = array |> LinearSearch.tryFindIndex element
     expected = result
+
+[<Property>]
+let ``Binary search works correctly`` element (array:int[]) =
+    let contains = array |> Array.contains element
+    let sortedArray = array |> Array.sort
+
+    let result = sortedArray |> BinarySearch.tryFindIndex element
+
+    match contains, result with
+    | true, Some index -> sortedArray.[index] = element
+    | false, None -> true
+    | _ -> false
