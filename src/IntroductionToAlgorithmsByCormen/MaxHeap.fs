4acebb0c153bd93286671a61e2d73cd32f53922b
Vladislav Timakov <jrmnnl@gmail.com>
2019-08-20 01:56:29 +0300

    Change Heap internals

--- src/IntroductionToAlgorithmsByCormen/MaxHeap.fs
+++ src/IntroductionToAlgorithmsByCormen/MaxHeap.fs
@@ -1,10 +1,9 @@
 ﻿namespace IntroductionToAlgorithmsByCormen
 
-open System
+open System.Linq
 
 type MaxHeap(array:int[]) =
-    let mutable _heapSize = array.Length
-    let _array = Array.copy array
+    let _array = array.ToList()
 
     let left i = ((i+1)*2)-1
     let right i = (i+1)*2
@@ -13,9 +12,9 @@ type MaxHeap(array:int[]) =
         let left = left i
         let right = right i
         let mutable largest = i
-        if left < _heapSize && _array.[left] > _array.[i] then
+        if left < _array.Count && _array.[left] > _array.[i] then
             largest <- left
-        if right < _heapSize && _array.[right] > _array.[largest] then
+        if right < _array.Count && _array.[right] > _array.[largest] then
             largest <- right
         if largest <> i then
             let temp = _array.[i]
@@ -24,16 +23,14 @@ type MaxHeap(array:int[]) =
             maxHeapify largest
 
     let ctor() =
-        for i = (_array.Length - 1) / 2 downto 0 do
+        for i = (_array.Count - 1) / 2 downto 0 do
             maxHeapify i
     do ctor()
-    
-    member this.HeapSize = _heapSize
 
     /// Removes root element
     member this.Pop() =
         let temp = _array.[0]
-        _array.[0] <- _array.[_heapSize-1]
-        _heapSize <- _heapSize - 1;
+        _array.[0] <- _array.[_array.Count]
+        _array.RemoveAt(_array.Count - 1)
         maxHeapify 0
         temp
